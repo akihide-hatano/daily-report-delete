d2a0f4d9744175de242e5cfe40bcb6eccee3577b
akihide-hatano <a.h.19841202@gmail.com>
2025-03-06 11:01:08 +0900

    firebaseの連携をしました。

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0CmysjSDFEMKYeBIKFCd3sU-jCZjjU1U",
+  authDomain: "daily-report-delete.firebaseapp.com",
+  projectId: "daily-report-delete",
+  storageBucket: "daily-report-delete.firebasestorage.app",
+  messagingSenderId: "284631210854",
+  appId: "1:284631210854:web:e4fe75885d39a228e846a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
